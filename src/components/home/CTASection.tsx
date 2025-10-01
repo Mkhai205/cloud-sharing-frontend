0f7095a637ed06ced015303dde6ac2ff1d03d147
MKhai205 <manhkhai123459876@gmail.com>
2025-10-01 23:53:48 +0700

    feat: implement CTA and Footer sections with responsive design

--- src/components/home/CTASection.tsx
+++ src/components/home/CTASection.tsx
@@ -1,3 +1,19 @@
 export default function CTASection() {
-    return <div>CTASection</div>;
+    return (
+        <div className="bg-amber-50 min-h-96 flex items-center">
+            <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:flex-1 lg:items-center lg:justify-around">
+                <h2 className="text-center text-3xl font-extrabold tracking-tight text-slate-950 sm:text-4xl">
+                    <span className="block">Ready to get started?</span>
+                    <span className="block text-amber-500">Create your account tody.</span>
+                </h2>
+                <div className="mt-8 flex justify-center lg:mt-0 flex-shrink-0">
+                    <div className="inline-flex rounded-md shadow">
+                        <button className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-amber-500 bg-white hover:text-slate-50 hover:bg-amber-500 hover:scale-105 transition-all duration-200 cursor-pointer">
+                            Sign up for free
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
--- src/components/home/FooterSection.tsx
+++ src/components/home/FooterSection.tsx
@@ -1,3 +1,11 @@
 export default function FooterSection() {
-    return <div>FooterSection</div>;
+    return (
+        <footer className="bg-slate-900">
+            <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
+                <div className="text-center text-base text-slate-400">
+                    &copy; 2025 Cloud Sharing. All rights reserved.
+                </div>
+            </div>
+        </footer>
+    );
 }
